4c07275d44be1a458d427663341b5cbb070c1602
Michael Frain <michael.frain@mobileforming.com>
2014-01-25 16:03:30 -0800

    Initial Commit

---
+++ HeadlineModule/HHMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HHMAppDelegate.h
+//  HeadlineModule
+//
+//  Created by Michael Frain on 1/25/14.
+//  Copyright (c) 2014 MFrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
