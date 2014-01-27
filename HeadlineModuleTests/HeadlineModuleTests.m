83fe833572074c737e256c8fafdd9b4bdaed5da6
Michael Frain <michael.frain@mobileforming.com>
2014-01-26 22:55:11 -0800

    adding basic unit tests to cover model building

--- HeadlineModuleTests/HeadlineModuleTests.m
+++ HeadlineModuleTests/HeadlineModuleTests.m
@@ -7,8 +7,17 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HHMRequestManager.h"
+#import "HHMViewController.h"
+#import "HHMHeadlineArray.h"
+#import "HHMHeadline.h"
+#import "HHMLinks.h"
+#import "HHMMobile.h"
+
 
 @interface HeadlineModuleTests : XCTestCase
+
+@property (nonatomic, strong) HHMHeadlineArray *testHeadlineArray;
 
 @end
 
@@ -26,9 +35,17 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testModelBuilding {
+    HHMRequestManager *requestManager = [HHMRequestManager sharedManager];
+    [requestManager headlineOfType:@"sports" andSport:nil success:^(HHMHeadlineArray *headline) {
+        self.testHeadlineArray = headline;
+        NSLog(@"Test is Ready.");
+        XCTAssertNotNil(self.testHeadlineArray, @"Data was not returned in the models properly.");
+        HHMHeadline *testHeadline = [self.testHeadlineArray.headlines objectAtIndex:4];
+        XCTAssertNotNil(testHeadline.links.mobile.href, @"Mobile Link model is not populating correctly.");
+    } failure:^(NSError *error) {
+        NSLog(@"Test is NOT Ready.");
+    }];
 }
 
 @end
